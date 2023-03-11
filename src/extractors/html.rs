8b852b2e3e673353a46ba36490d2eed8a695345b
Robin Vobruba <hoijui.quaero@gmail.com>
2023-03-11 11:37:08 +0100

    html: Improves performance, cleanliness (a bit) and fixes bugs [fix] [major]

--- src/extractors/html.rs
+++ src/extractors/html.rs
@@ -24,6 +24,150 @@ enum Attribute {
     Href,
     Name,
     Id,
+    Other,
+}
+
+struct Scanner<'a> {
+    line: &'a str,
+    chars: Vec<char>,
+    column: usize,
+}
+
+impl<'a> Scanner<'a> {
+    pub const fn empty() -> Self {
+        Scanner {
+            line: "",
+            chars: vec![],
+            column: 0,
+        }
+    }
+
+    pub fn reset(&mut self, new_line: &'a str) {
+        self.line = new_line;
+        self.chars = new_line.chars().collect();
+        self.column = 0;
+    }
+
+    pub fn take(&mut self, token: &str) -> bool {
+        let found = self
+            .line
+            .get(self.column..self.column + token.len())
+            .map_or(false, |slc| slc.eq(token));
+        if found {
+            self.column += token.len();
+        }
+        found
+    }
+
+    pub fn take_and_ws(&mut self, token: &str) -> bool {
+        let mut found = self
+            .line
+            .get(self.column..self.column + token.len())
+            .map_or(false, |slc| slc.eq(token));
+        if found {
+            found = false;
+            let mut count = 0;
+            while let Some(chr) = self.chars.get(self.column + token.len() + count) {
+                if chr.is_whitespace() {
+                    count += 1;
+                } else {
+                    break;
+                }
+            }
+            if count > 0 || self.is_done() {
+                self.column += token.len();
+                found = true;
+            }
+        }
+        found
+    }
+
+    pub fn take_single(&mut self, token: char) -> bool {
+        let found = self
+            .chars
+            .get(self.column)
+            .map_or(false, |slc| slc.eq(&token));
+        if found {
+            self.column += 1;
+        }
+        found
+    }
+
+    pub fn take_any(&mut self) -> Option<char> {
+        let chr = self.chars.get(self.column);
+        if chr.is_some() {
+            self.column += 1;
+        }
+        chr.copied()
+    }
+
+    pub fn skip_ws(&mut self) -> bool {
+        let mut count = 0;
+        while let Some(chr) = self.chars.get(self.column + count) {
+            if chr.is_whitespace() {
+                count += 1;
+            } else {
+                break;
+            }
+        }
+        self.column += count;
+        count > 0
+    }
+
+    pub fn is_non_ws(&self) -> bool {
+        if let Some(chr) = self.chars.get(self.column) {
+            if chr.is_whitespace() {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn take_non_ws(&mut self) -> &'a str {
+        let mut count = 0;
+        while let Some(chr) = self.chars.get(self.column + count) {
+            if chr.is_whitespace() {
+                break;
+            }
+            count += 1;
+        }
+        self.column += count;
+        &self.line[self.column - count..self.column]
+    }
+
+    pub fn take_non_ws_or(&mut self, token: char) -> &'a str {
+        let mut count = 0;
+        while let Some(chr) = self.chars.get(self.column + count) {
+            if chr.is_whitespace() || chr == &token {
+                break;
+            }
+            count += 1;
+        }
+        self.column += count;
+        &self.line[self.column - count..self.column]
+    }
+
+    pub fn take_non(&mut self, token: char) -> Option<&'a str> {
+        let mut count = 0;
+        let mut found = false;
+        while let Some(chr) = self.chars.get(self.column + count) {
+            if chr == &token {
+                found = true;
+                break;
+            }
+            count += 1;
+        }
+        if found || count > 0 {
+            self.column += count;
+            Some(&self.line[self.column - count..self.column])
+        } else {
+            None
+        }
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.column >= self.chars.len()
+    }
 }
 
 impl super::LinkExtractor for LinkExtractor {
@@ -38,106 +182,85 @@ impl super::LinkExtractor for LinkExtractor {
         let mut state: ParserState = ParserState::Text;
         let mut is_anchor = false;
         // let mut element_part: Option<Attribute>;
+        let mut scanner = Scanner::empty();
         for (line, line_str) in file.content.fetch()?.as_ref().lines().enumerate() {
-            let line_chars: Vec<char> = line_str.chars().collect();
-            let mut column: usize = 0;
-            while line_chars.get(column).is_some() {
+            scanner.reset(line_str);
+            while !scanner.is_done() {
                 match state {
                     ParserState::Comment => {
-                        if line_chars.get(column) == Some(&'-')
-                            && line_chars.get(column + 1) == Some(&'-')
-                            && line_chars.get(column + 2) == Some(&'>')
-                        {
-                            column += 2;
+                        if scanner.take("-->") {
                             state = ParserState::Text;
+                        } else {
+                            scanner.take_any();
                         }
                     }
                     ParserState::Text => {
-                        if line_chars.get(column) == Some(&'<')
-                            && line_chars.get(column + 1) == Some(&'!')
-                            && line_chars.get(column + 2) == Some(&'-')
-                            && line_chars.get(column + 3) == Some(&'-')
-                        {
-                            column += 3;
+                        if scanner.take("<!--") {
                             state = ParserState::Comment;
-                        } else if line_chars.get(column) == Some(&'<')
-                            && line_chars.get(column + 1) == Some(&'a')
-                        {
-                            column += 1;
-                            state = ParserState::Element;
-                            is_anchor = true;
-                        } else if line_chars.get(column) == Some(&'<')
-                            && line_chars.get(column + 1) != Some(&'a')
-                        {
-                            column += 1;
-                            while line_chars.get(column).is_some()
-                                && !line_chars[column].is_whitespace()
-                            {
-                                column += 1;
+                        } else if scanner.take_single('<') {
+                            // not "<a"!
+                            scanner.skip_ws();
+                            let _end = scanner.take_single('/');
+                            let elem = scanner.take_non_ws_or('>');
+                            is_anchor = elem == "a";
+                            if !scanner.take_single('>') {
+                                state = ParserState::Element;
+                                scanner.skip_ws();
                             }
-                            state = ParserState::Element;
-                            is_anchor = false;
+                        } else {
+                            scanner.take_any();
                         }
                     }
                     ParserState::Element => {
-                        if is_anchor {
-                            if conf.extract_links()
-                                && line_chars.get(column) == Some(&'h')
-                                && line_chars.get(column + 1) == Some(&'r')
-                                && line_chars.get(column + 2) == Some(&'e')
-                                && line_chars.get(column + 3) == Some(&'f')
-                            {
-                                column += 3;
-                                state = ParserState::EqualSign;
-                                attribute = Some(Attribute::Href);
-                            } else if conf.extract_anchors()
-                                && line_chars.get(column) == Some(&'n')
-                                && line_chars.get(column + 1) == Some(&'a')
-                                && line_chars.get(column + 2) == Some(&'m')
-                                && line_chars.get(column + 3) == Some(&'e')
-                            {
-                                column += 3;
-                                state = ParserState::EqualSign;
-                                attribute = Some(Attribute::Name);
+                        scanner.skip_ws();
+                        if scanner.take_single('>') {
+                            state = ParserState::Text;
+                        } else if let Some(attrib_name) = scanner.take_non('=') {
+                            debug!("attrib_name: '{attrib_name}'");
+                            match attrib_name {
+                                "href" if is_anchor && conf.extract_links() => {
+                                    state = ParserState::EqualSign;
+                                    attribute = Some(Attribute::Href);
+                                }
+                                "name" if is_anchor && conf.extract_anchors() => {
+                                    state = ParserState::EqualSign;
+                                    attribute = Some(Attribute::Name);
+                                }
+                                "id" if conf.extract_anchors() => {
+                                    state = ParserState::EqualSign;
+                                    attribute = Some(Attribute::Id);
+                                }
+                                _ => {
+                                    state = ParserState::EqualSign;
+                                    attribute = Some(Attribute::Other);
+                                }
                             }
-                        }
-                        if conf.extract_anchors()
-                            && line_chars.get(column) == Some(&'i')
-                            && line_chars.get(column + 1) == Some(&'d')
-                        {
-                            column += 1;
-                            state = ParserState::EqualSign;
-                            attribute = Some(Attribute::Id);
+                            scanner.skip_ws();
+                        } else {
+                            panic!("Bad HTML: Can't have empty attribute name!");
                         }
                     }
                     ParserState::EqualSign => {
-                        match line_chars.get(column) {
-                            Some(x) if x.is_whitespace() => {}
-                            Some(x) if x == &'=' => state = ParserState::Attribute,
-                            Some(_) => state = ParserState::Element,
-                            None => {}
-                        };
+                        scanner.skip_ws();
+                        if scanner.take_single('=') {
+                            state = ParserState::Attribute;
+                        } else if scanner.is_non_ws() {
+                            panic!(
+                                "Bad character encountered while in state {state:#?}: {:#?}",
+                                scanner.take_any()
+                            );
+                        }
                     }
                     ParserState::Attribute => {
+                        scanner.skip_ws();
                         if let Some(attrib_cont) = attribute {
-                            match line_chars.get(column) {
-                                Some(x) if !x.is_whitespace() && x != &'"' => {
-                                    let attrib_column = column;
-                                    while line_chars.get(column).is_some()
-                                        && !line_chars[column].is_whitespace()
-                                        && line_chars[column] != '"'
-                                    {
-                                        column += 1;
-                                    }
-                                    while let Some(c) = line_chars.get(column) {
-                                        if c.is_whitespace() || c == &'"' {
-                                            break;
-                                        }
-                                        column += 1;
-                                    }
-                                    let attrib_target = &line_chars[attrib_column..column]
-                                        .iter()
-                                        .collect::<String>();
+                            match scanner.take_any() {
+                                Some('"') => {
+                                    let attrib_column = scanner.column;
+                                    let attrib_target = scanner.take_non('"').expect(
+                                        "Bad HTML! need to finnish attribute value with '\"' (Note: We do not support multi-line attribute values (yet)!)",
+                                    );
+                                    scanner.take_single('"');
                                     let pos = Position {
                                         line: line + 1,
                                         column: attrib_column + 1,
@@ -153,7 +276,7 @@ impl super::LinkExtractor for LinkExtractor {
                                                 file: file.locator.clone(),
                                                 pos,
                                             },
-                                            name: attrib_target.clone(),
+                                            name: attrib_target.to_string(),
                                             r#type: anchor::Type::Direct,
                                         }),
                                         Attribute::Id => anchors.push(Anchor {
@@ -161,18 +284,19 @@ impl super::LinkExtractor for LinkExtractor {
                                                 file: file.locator.clone(),
                                                 pos,
                                             },
-                                            name: attrib_target.clone(),
+                                            name: attrib_target.to_string(),
                                             r#type: anchor::Type::ElementId,
                                         }),
+                                        Attribute::Other => {}
                                     }
-                                    state = ParserState::Text;
+                                    state = ParserState::Element;
+                                    attribute = None;
                                 }
                                 Some(_) | None => {}
                             };
                         }
                     }
                 }
-                column += 1;
             }
         }
         Ok((links, anchors))
@@ -204,6 +328,51 @@ mod tests {
         };
         let markup_file = File::dummy(content, Type::Html);
         super::super::find_links(&markup_file, &conf).map(|(_links, anchors)| anchors)
+    }
+
+    #[test]
+    fn sc_take() -> std::io::Result<()> {
+        let mut scanner = Scanner::empty();
+        scanner.reset("Hello World");
+        assert!(scanner.take("Hello"));
+        assert!(scanner.take_single(' '));
+        assert!(scanner.take("World"));
+        assert!(scanner.is_done());
+        Ok(())
+    }
+
+    #[test]
+    fn sc_take_any() -> std::io::Result<()> {
+        let mut scanner = Scanner::empty();
+        scanner.reset("Hello World");
+        assert!(scanner.take("Hello"));
+        assert_eq!(scanner.take_any(), Some(' '));
+        assert!(scanner.take("World"));
+        assert_eq!(scanner.take_any(), None);
+        assert!(scanner.is_done());
+        Ok(())
+    }
+
+    #[test]
+    fn sc_skip_ws() -> std::io::Result<()> {
+        let mut scanner = Scanner::empty();
+        scanner.reset("Hello \t \n\r\n \t World");
+        assert!(scanner.take("Hello"));
+        assert!(scanner.skip_ws());
+        assert!(scanner.take("World"));
+        assert!(scanner.is_done());
+        Ok(())
+    }
+
+    #[test]
+    fn sc_non_ws() -> std::io::Result<()> {
+        let mut scanner = Scanner::empty();
+        scanner.reset("Hello World");
+        assert_eq!(scanner.take_non_ws(), "Hello");
+        assert!(scanner.take_single(' '));
+        assert_eq!(scanner.take_non_ws(), "World");
+        assert!(scanner.is_done());
+        Ok(())
     }
 
     #[test]
